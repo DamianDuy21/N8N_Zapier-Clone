afea3561fbb21a031c7e900ad74f36a135861202
DamianDuy21 <damianduy21@gmail.com>
2025-10-18 16:35:17 +0700

    first commit

---
+++ nodebase/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div>
+      <Button className="">Hello, World!</Button>
+    </div>
+  );
+};
+
+export default Page;
