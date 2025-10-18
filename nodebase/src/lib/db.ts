c49ce6e7efc76dd847e9e9b7af6d8bab105e658e
DamianDuy21 <damianduy21@gmail.com>
2025-10-18 19:54:30 +0700

    init database

--- nodebase/src/lib/db.ts
+++ nodebase/src/lib/db.ts
@@ -6,6 +6,8 @@ const globalForPrisma = global as unknown as {
 
 const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
+// Ensure the prisma client is re-used during development to prevent exhausting database connections.
+// production code should not use this pattern because there is no hot reloading.
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
